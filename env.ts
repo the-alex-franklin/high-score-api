6e56c10f63c0c2283c6e102300135c3684721b9b
Andy <the.andy.franklin@gmail.com>
2024-05-23 10:27:20 -0700

    refactored env stuff a bit

--- env.ts
+++ env.ts
@@ -1,15 +1,11 @@
-import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
-import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts';
+import { load } from "dotenv";
+import { z } from 'zod';
 
 const env = await load();
 
 const validated_env = z.object({
   MONGO_ATLAS_USERNAME: z.string(),
   MONGO_ATLAS_PASSWORD: z.string(),
-})
-  .transform(Object.entries)
-  .parse(env);
+}).parse(env);
 
-for (const [key, value] of validated_env) {
-  Deno.env.set(key, value);
-}
+export { validated_env as env }
--- index.ts
+++ index.ts
@@ -1,17 +1,13 @@
-import './env.ts';
-import { Hono } from 'https://deno.land/x/hono@v4.3.10/mod.ts';
+import { Hono } from 'hono/mod.ts';
+import { cors } from 'hono/middleware.ts'
 import { connect } from 'mongoose';
 import { Score } from "./score.model.ts";
+import { env } from "./env.ts";
 
 const app = new Hono();
+app.use(cors());
 
-const mongo_atlas_username = Deno.env.get('MONGO_ATLAS_USERNAME');
-const mongo_atlas_password = Deno.env.get('MONGO_ATLAS_PASSWORD');
-
-if (!mongo_atlas_username) throw new Error('MONGO_ATLAS_USERNAME is required');
-if (!mongo_atlas_password) throw new Error('MONGO_ATLAS_PASSWORD is required');
-
-connect(`mongodb+srv://${mongo_atlas_username}:${mongo_atlas_password}@cluster0.ycjf2yc.mongodb.net/high-score`)
+connect(`mongodb+srv://${env.MONGO_ATLAS_USERNAME}:${env.MONGO_ATLAS_PASSWORD}@cluster0.ycjf2yc.mongodb.net/high-score`)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error(err));
 
